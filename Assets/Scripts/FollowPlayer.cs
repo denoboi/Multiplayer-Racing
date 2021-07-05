515923f54474c942d8ff6ebb9c7ee1bff7254c2a
denoboi <deniz.kaymakci@hotmail.com>
2021-07-05 17:04:55 +0300

    Cam

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset = new Vector3(0,8,-17);
+    
+    
+    void Start()
+    {
+       
+    }
+
+   
+    void LateUpdate()
+    {
+        //kameranin pozisyonu ile player pozisyonu esitleniyor.
+        transform.position = player.transform.position + offset;
+
+       
+       
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public float turnSpeed = 15;
+    private float horizontalInput;
+    private float verticalInput;
+    public Camera cam1;
+    public Camera cam2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        
+
+        //Move the vehicle forward 
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed) ;
+        //rotate the vehicle 
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime );
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cam1.enabled = !cam1.enabled;
+            cam2.enabled = !cam2.enabled;
+        }
+       
+
+    }
+}
