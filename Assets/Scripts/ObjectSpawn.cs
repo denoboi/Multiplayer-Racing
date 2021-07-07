bce6e5a12742bb4b031f5df991bee9e438d48e7c
denoboi <deniz.kaymakci@hotmail.com>
2021-07-07 16:49:12 +0300

    Tank

---
+++ Assets/Scripts/ObjectSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawn : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    private Vector3 spawnPos = new Vector3(-7, 0, -3);
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ballPrefab, spawnPos, ballPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
